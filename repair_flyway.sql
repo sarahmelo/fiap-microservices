adb3be0df0aa31d03e78e9af36db92472fd2708f
Sarah Melo <sarahcbdg@gmail.com>
2025-04-19 22:26:58 -0300

    build: docker configuratiojs

---
+++ repair_flyway.sql
@@ -0,0 +1,5 @@
+-- Limpar e reparar a tabela de histórico do Flyway para users
+DROP TABLE IF EXISTS tbl_flyway_schema_history_users;
+
+-- Limpar e reparar a tabela de histórico do Flyway para sprinkler
+DROP TABLE IF EXISTS tbl_flyway_schema_history_sprinkler;
