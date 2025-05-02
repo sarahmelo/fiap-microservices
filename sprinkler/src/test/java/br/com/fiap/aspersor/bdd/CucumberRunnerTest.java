3fc88240a4cfc7e62d8367824f22fbdb23ab53a8
Sarah Melo <sarahcbdg@gmail.com>
2025-05-02 10:33:43 -0300

    fix: test worklow

--- sprinkler/src/test/java/br/com/fiap/aspersor/bdd/CucumberRunnerTest.java
+++ sprinkler/src/test/java/br/com/fiap/aspersor/bdd/CucumberRunnerTest.java
@@ -1,14 +1,15 @@
 package br.com.fiap.aspersor.bdd;
 
-import org.junit.runner.RunWith;
+// Mantendo compatibilidade com JUnit 4 para facilitar transição
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = {"br.com.fiap.aspersor.bdd.steps"},
-    plugin = {"pretty", "html:target/cucumber-reports"}
+    plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber-reports/cucumber.json"}
 )
 public class CucumberRunnerTest {
 }
